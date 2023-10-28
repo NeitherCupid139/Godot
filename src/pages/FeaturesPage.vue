01c761509fa3bdf9b53c1330e020a542e65558c1
monster <2248011005@qq.com>
2023-10-28 08:18:50 +0000

    update src/pages/FeaturesPage.vue. delete some irrelevant ones
    Signed-off-by: monster <2248011005@qq.com>

--- src/pages/FeaturesPage.vue
+++ src/pages/FeaturesPage.vue
@@ -363,42 +363,6 @@
       </div>
     </div>
   </main>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  <div id="carouselExample" class="carousel slide">
-    <div class="carousel-inner">
-      <div class="carousel-item active">
-        <img src="../assets/images/carousel_01.png" class="d-block w-100" alt="...">
-      </div>
-      <div class="carousel-item">
-        <img src="../assets/images/carousel_03.jpg" class="d-block w-100" alt="...">
-      </div>
-      <div class="carousel-item">
-        <img src="../assets/images/carousel_06.jpg" class="d-block w-100" alt="...">
-      </div>
-    </div>
-    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
-      <!-- <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Previous</span> -->
-    </button>
-    <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
-      <!-- <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Next</span> -->
-    </button>
-  </div>
 </template>
 
 <script setup>
